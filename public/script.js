5ec2fc094e97d55c359431c74040db3916d196f3
Yurika Namba <yurikanamba@MacBook-Pro.local>
2020-07-13 12:28:33 +0900

    readded feed

--- public/script.js
+++ public/script.js
@@ -1,28 +1,28 @@
 const photoFeed = document.querySelector(".photo-feed");
 
 //TOGGLE
-// const playground = document.getElementById("playground");
-// const feed = document.getElementById("feed");
-// const toggleBtn = document.getElementById("toggle");
-// const spheresArr = document.getElementsByClassName("sphere");
-// console.log(spheresArr.length);
+const playground = document.getElementById("playground");
+const feed = document.getElementById("feed");
+const toggleBtn = document.getElementById("toggle");
+const spheresArr = document.getElementsByClassName("sphere");
+console.log(spheresArr.length);
 
-// toggleBtn.addEventListener("click", (e) => {
-//   if (e.target.value === "feed") {
-//     console.log(document.getElementById("feed").style.display);
-//     document.getElementById("feed").style.display = "block";
-//     document.getElementById("playground").style.display = "none";
-//     document.getElementById("toggle").innerHTML = "Playground";
-//     document.getElementById("toggle").value = "playground";
-//   } else {
-//     document.getElementById("playground").style.display = "block";
-//     document.getElementById("feed").style.display = "none";
-//     document.getElementById("toggle").innerHTML = "Feed";
-//     document.getElementById("toggle").value = "feed";
-//   }
-// });
+toggleBtn.addEventListener("click", (e) => {
+  if (e.target.value === "feed") {
+    console.log(document.getElementById("feed").style.display);
+    document.getElementById("feed").style.display = "block";
+    document.getElementById("playground").style.display = "none";
+    document.getElementById("toggle").innerHTML = "Playground";
+    document.getElementById("toggle").value = "playground";
+  } else {
+    document.getElementById("playground").style.display = "block";
+    document.getElementById("feed").style.display = "none";
+    document.getElementById("toggle").innerHTML = "Feed";
+    document.getElementById("toggle").value = "feed";
+  }
+});
 
-//UPLOAD PHOT TO FIREBASE
+//UPLOAD PHOTO TO FIREBASE
 window.addEventListener("scroll", () => {
   document.getElementById("fixed-btn").style.display = "block";
 });
@@ -69,7 +69,7 @@ photos.listAll().then((result) => {
       .getDownloadURL()
       .then((url) => {
         photoURLs.push(url);
-        //init2(url);
+        init2(url);
         return photoURLs;
       })
       .then((photoURLs) => {
@@ -171,85 +171,85 @@ function render() {
 }
 
 //FEED
-// function init2(url) {
-//   let renderer = new THREE.WebGLRenderer();
-//   renderer.domElement.classList.add("sphere");
-//   //to make objects sharper edges you can anti-alias
-//   //var renderer = new THREE.WebGLRenderer({antialias: true});
-//   renderer.setSize(window.innerWidth / 2, window.innerHeight / 2, false);
+function init2(url) {
+  let renderer = new THREE.WebGLRenderer();
+  renderer.domElement.classList.add("sphere");
+  //to make objects sharper edges you can anti-alias
+  //var renderer = new THREE.WebGLRenderer({antialias: true});
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2, false);
 
-//   //you can set background color
-//   //renderer.setClearColor(0x140b33, 1);
-//   const feedContainer = document.getElementById("feed");
-//   feedContainer.appendChild(renderer.domElement);
+  //you can set background color
+  //renderer.setClearColor(0x140b33, 1);
+  const feedContainer = document.getElementById("feed");
+  feedContainer.appendChild(renderer.domElement);
 
-//   //allow you to set up what and where is to be rendered
-//   //where you place objects, lights and cameras
-//   let scene = new THREE.Scene();
-//   scene.background = new THREE.Color(0xffffff);
-//   let camera = new THREE.PerspectiveCamera(
-//     //field of view is the extent of the sene that is seen on the display at any given moment
-//     //the value is in degrees
-//     75,
-//     //aspect ratio
-//     window.innerWidth / window.innerHeight,
-//     //near clipping plane
-//     0.1,
-//     //far clipping plane
-//     1000
-//     //objects further away from the cammera than the value of far or closer than near won't be rendered
-//   );
+  //allow you to set up what and where is to be rendered
+  //where you place objects, lights and cameras
+  let scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xffffff);
+  let camera = new THREE.PerspectiveCamera(
+    //field of view is the extent of the sene that is seen on the display at any given moment
+    //the value is in degrees
+    75,
+    //aspect ratio
+    window.innerWidth / window.innerHeight,
+    //near clipping plane
+    0.1,
+    //far clipping plane
+    1000
+    //objects further away from the cammera than the value of far or closer than near won't be rendered
+  );
 
-//   //camera.position.set(0, 20, 100);
-//   camera.position.z = 10;
-//   //   camera.position.set(100, -400, 2000);
-//   //   adding camera to the scene
-//   //   scene.add(camera);
+  //camera.position.set(0, 20, 100);
+  camera.position.z = 10;
+  //   camera.position.set(100, -400, 2000);
+  //   adding camera to the scene
+  //   scene.add(camera);
 
-//   //an object that contains all the vertices and faces of the cube
-//   //depth, width, height
-//   // const geometry = new THREE.BoxGeometry(1, 1, 1);
-//   const geometry = new THREE.SphereGeometry(4, 32, 32);
-//   //material to colour the cube
-//   //const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-//   //   const material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./anish_kapoor.jpg') });
+  //an object that contains all the vertices and faces of the cube
+  //depth, width, height
+  // const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const geometry = new THREE.SphereGeometry(4, 32, 32);
+  //material to colour the cube
+  //const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  //   const material = new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture('./anish_kapoor.jpg') });
 
-//   //to set a texture to material
-//   const texture = new THREE.TextureLoader().load(url);
-//   const material = new THREE.MeshBasicMaterial({ map: texture });
+  //to set a texture to material
+  const texture = new THREE.TextureLoader().load(url);
+  const material = new THREE.MeshBasicMaterial({ map: texture });
 
-//   //mesh takes a geometry and applies a material to it
-//   //cube = new THREE.Mesh(geometry, material);
+  //mesh takes a geometry and applies a material to it
+  //cube = new THREE.Mesh(geometry, material);
 
-//   let sphere = new THREE.Mesh(geometry, material);
+  let sphere = new THREE.Mesh(geometry, material);
 
-//   //insert into scene
-//   //automatically added to coordinates (0, 0, 0)
-//   //this causes both camera and cube to be inside each other so move the camera out
-//   //scene.add(cube);
-//   scene.add(sphere);
+  //insert into scene
+  //automatically added to coordinates (0, 0, 0)
+  //this causes both camera and cube to be inside each other so move the camera out
+  //scene.add(cube);
+  scene.add(sphere);
 
-//   //ANIMATE THE SPHERE
-//   const animate = function () {
-//     //pauses when user navigates to another browser tab
-//     requestAnimationFrame(animate);
+  //ANIMATE THE SPHERE
+  const animate = function () {
+    //pauses when user navigates to another browser tab
+    requestAnimationFrame(animate);
 
-//     //will be run every frame and gives rotation animation
-//     //changes numbers to change speed of rotation
-//     sphere.rotation.x += 0.01;
-//     sphere.rotation.y += 0.01;
-//     renderer.render(scene, camera);
-//   };
-//   animate();
+    //will be run every frame and gives rotation animation
+    //changes numbers to change speed of rotation
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  };
+  animate();
 
-//   //resize the renderer when window resizes
-//   function onWindowResize() {
-//     //set camera fresttrum? ratio
-//     camera.aspect = window.innerWidth / window.innerHeight;
-//     //update camera
-//     camera.updateProjectionMatrix();
-//     //set renderer size
-//     renderer.setSize(window.innerWidth / 2, window.innerHeight / 2, false);
-//   }
-//   window.addEventListener("resize", onWindowResize);
-// }
+  //resize the renderer when window resizes
+  function onWindowResize() {
+    //set camera fresttrum? ratio
+    camera.aspect = window.innerWidth / window.innerHeight;
+    //update camera
+    camera.updateProjectionMatrix();
+    //set renderer size
+    renderer.setSize(window.innerWidth / 2, window.innerHeight / 2, false);
+  }
+  window.addEventListener("resize", onWindowResize);
+}
